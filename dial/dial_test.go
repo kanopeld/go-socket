6f5185d8876c204f15eae03b1d220c05ebda6be8
kanopeld <968597@gmail.com>
2019-12-03 17:38:14 +0300

    tests stub add

--- dial/dial_test.go
+++ dial/dial_test.go
@@ -1,1 +1,13 @@
 package dial
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestNewDial(t *testing.T) {
+	convey.Convey("testing get dial", t, func() {
+		_, err := NewDial(":81")
+		convey.So(err, convey.ShouldNotBeNil)
+	})
+}
--- server/client_test.go
+++ server/client_test.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	convey.Convey("testing get new server", t, func() {
+		_, err := NewServer(":81881")
+		convey.So(err, convey.ShouldNotBeNil)
+	})
+}
