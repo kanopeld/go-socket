4e8acd8630e33f624eb1c179aede145be7a7ec72
shilangyu <xmarcinmarcin@gmail.com>
2019-12-07 15:32:14 +0100

    simplified interfaces

--- interfaces.go
+++ interfaces.go
@@ -2,47 +2,36 @@ package socket
 
 import "net"
 
-type IDer interface {
-	// ID return the socket id
+type ider interface {
+	// ID returns the socket id
 	ID() string
 }
 
-type Connectioner interface {
-	// Connection return net.Conn with which the socket was created
-	Connection() net.Conn
-}
-
-type Handler interface {
-	// On registers an event handler under the given name.
-	On(event string, c HandlerCallback)
-	// Off deletes an event handler. Return true if event was exist
-	Off(event string) bool
-}
-
-type Disconnecter interface {
-	// Disconnect drop current connection. Send the appropriate message to the other side
-	Disconnect()
-}
-
-// This interface is used as an extension of Emiiter
+// IdentifiableEmitter is an extension of Emiter
 type IdentifiableEmitter interface {
-	IDer
+	ider
 	// Emitter organizes sending events to the other side
 	Emitter
 }
 
-// Basic client interface. Includes all basic functions
+// Client includes all basic functions for a client
 type Client interface {
 	Emitter
-	IDer
-	Connectioner
-	Handler
-	Disconnecter
+	ider
 	Broadcaster
+	// Connection returns net.Conn with which the socket was created
+	Connection() net.Conn
+	// Disconnect drops current connection. Sends the appropriate message to the other side
+	Disconnect()
+	// On registers an event handler under the given name
+	On(event string, c HandlerCallback)
+	// Off deletes an event handler. Return true if event existed
+	Off(event string) bool
 }
 
 type looper interface {
 	loop()
 }
 
+// HandlerCallback is function that gets called on a certain event
 type HandlerCallback func(c Client, data []byte) error
